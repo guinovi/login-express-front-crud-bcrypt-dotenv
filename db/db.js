8a4581ee0996ada3272677998b296246bec82718
guinovi <gui.novillo@gmail.com>
2024-07-11 13:48:27 -0300

    pre deploy config .env y db

--- db/db.js
+++ db/db.js
@@ -12,6 +12,7 @@ const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     multipleStatements: true
 };
 
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ const PORT = process.env.PORT || 3000;
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en puerto:${PORT} 
+        o http://localhost:${PORT}`);
 });
